cced1fa1032d788eab25ae6e6a28973dec9ea532
Katerina Ohnieva <brilliantskate2588@gmail.com>
2026-03-21 23:58:45 +0200

    changes-hw-js-11

--- src/main.js
+++ src/main.js
@@ -1,41 +1,46 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import { getImagesByQuery } from "./js/pixabay-api.js";
 import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions.js";
 
 const form = document.querySelector('.form');
+const API_KEY = '55082106-4cb46bb19233a000c0dff9eca'; 
 
-form.addEventListener('submit', event => {
+function getImagesByQuery(query) {
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&per_page=12`;
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) throw new Error('Network response was not ok');
+      return res.json();
+    });
+}
+
+form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const searchText = event.target.elements['search-text'].value.trim();
 
   if (searchText === '') {
-    iziToast.error({
-      message: 'Please enter a search query!',
-    });
+    iziToast.error({ message: 'Please enter a search query!' });
     return;
-  } 
+  }
 
-  clearGallery();     
-  showLoader();      
+  clearGallery();
+  showLoader();
 
   getImagesByQuery(searchText)
     .then(data => {
       if (!data.hits || data.hits.length === 0) {
-        iziToast.error({
-          message: 'Sorry, there are no images matching your search query. Please try again!',
-        });
+        iziToast.error({ message: 'Sorry, there are no images matching your search query. Please try again!' });
         return;
       }
 
-      createGallery(data.hits); 
-     iziToast.success({ message: `Found ${data.totalHits} images!` });
+      createGallery(data.hits);
+      iziToast.success({ message: `Found ${data.totalHits} images!` });
     })
     .catch(() => {
       iziToast.error({ message: 'Something went wrong!' });
     })
     .finally(() => {
-      hideLoader()
+      hideLoader();
     });
 });
